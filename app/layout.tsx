c3c4099d7abf6edacb46dd8541b599f4efdc0837
riteekagawand <riteekagawand7@gmail.com>
2026-03-13 13:44:46 +0530

    Project setup

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata = {
+  title: "Chatbot App",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={cn("font-sans", geist.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind v4 is finally working 🎉
+      </h1>
+    </div>
+  );
+}
